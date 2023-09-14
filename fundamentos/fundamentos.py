307dd1684b0807197f482aba2136c5aeb05baa29
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2023-09-13 23:53:36 -0300

    Operadores Aritiméticos

--- fundamentos/fundamentos.py
+++ fundamentos/fundamentos.py
@@ -37,3 +37,30 @@ print(salario - despesas)
 
 # print('Fim')
 print('Fim de verdade')  # comentário aqui também vale
+
+#  Operadors Aritméticos
+# Operadores Binários
+print(2 + 3)  # -> Soma
+print(4 - 7)  # -> Subtracao
+print(2 * 5.3)  # -> Multiplicação
+print(9.4 / 3)  # -> Divisão
+print(9.4 // 3)  # -> Divisão Inteira
+print(5 ** 8)  # -> Potenciacao
+print(10 % 3)  # -> Resto
+
+y = 5
+# Unário operador pré fixado
+print(++y)  # -> Soma
+# Unário operador pós fixado
+x = 5
+# print(x--)    # -> Subtracao
+
+
+# Desafio 1 Operadores Aritméticos
+# Minhas variáveis
+salario = 3450.45
+despesas = 2456.20
+
+# Resposta
+percentual_comprometido = despesas / salario * 100
+print(percentual_comprometido)
