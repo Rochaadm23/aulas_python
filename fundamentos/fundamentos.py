73890eaa55658a8c471038116d416339f6b94d94
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2023-09-13 23:24:23 -0300

    Tipos básicos em python

---
+++ fundamentos/fundamentos.py
@@ -0,0 +1,12 @@
+# Tipos Básicos
+
+print(True)  # -> Booleano
+print(False)  # -> Booleano
+print(1.2 + 1)  # -> Float
+print('Aqui eu falo na minha lingua')  # -> String
+print('também funciona')  # -> String
+print('você é ' + 3 * 'muito ' + 'legal')  # -> String
+# print(3 + 'a') -> Ambiguidade
+print([1, 2, 3])  # -> Lista
+print({'nome': 'Fernando', 'idade': 30, 'salário': 20000.00})  # -> Dicionário
+print(None)  # -> Nulo
---
+++ fundamentos/teste.py
@@ -0,0 +1,10 @@
+a = 2
+b = 3
+
+lista = [1, 2, 3]
+dicionario = {'nome': 'Fernando', 'idade': 32, 'salário': 20000.00}
+
+print('Olá mundo!')
+
+print(2 / 3)
+print(2 / 4)
