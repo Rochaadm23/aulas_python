a816a612bb6e4f26f41b48105ed3938e669e5ac8
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2023-09-13 23:36:38 -0300

    Tipos de variáveis e comentários

--- fundamentos/fundamentos.py
+++ fundamentos/fundamentos.py
@@ -10,3 +10,30 @@ print('você é ' + 3 * 'muito ' + 'legal')  # -> String
 print([1, 2, 3])  # -> Lista
 print({'nome': 'Fernando', 'idade': 30, 'salário': 20000.00})  # -> Dicionário
 print(None)  # -> Nulo
+
+# Tipos Variáveis
+
+a = 10
+b = 5.2
+print(a + b)
+
+a = 'agora sou uma string'
+print(a)
+
+# print(a + b)
+
+# Comentários de Código
+
+# Minhas variáveis
+salario = 3450.45
+despesas = 2456.20
+
+"""
+A ideia é calcular o quanto
+vai sobrar no final do mês
+
+"""
+print(salario - despesas)
+
+# print('Fim')
+print('Fim de verdade')  # comentário aqui também vale
